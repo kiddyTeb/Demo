fa1576c346a149dabf23f072e765f717e01e600f
kid <kid0916@163.com>
2016-08-29 23:43:29 +0800

    three

--- app/src/main/java/com/liangdekai/demo/activity/Test.java
+++ app/src/main/java/com/liangdekai/demo/activity/Test.java
@@ -1,7 +1,32 @@
 package com.liangdekai.demo.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.liangdekai.demo.R;
+
 /**
- * Created by asus on 2016/8/27.
+ * Created by asus on 2016/7/16.
  */
-public class Test {
+public class TestActivity extends Activity implements View.OnClickListener{
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.draw);
+        button = (Button) findViewById(R.id.ss);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ss :
+
+        }
+    }
 }
